ae4a4df4e979744bffc4f01edda7f0c81a80d967
noorjahan <noorjahan@N.Ferdous-ASUSVivoBook>
2022-11-18 21:44:33 +0600

    added my location

--- src/screens/Map/MapScreen.js
+++ src/screens/Map/MapScreen.js
@@ -1,28 +1,42 @@
 import * as React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 
 const { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
-const LATITUDE = 24.903561;
-const LONGITUDE = 91.873611
+const LATITUDE = 24.726236;
+const LONGITUDE = 91.756729;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-     <MapView
-            provider={PROVIDER_GOOGLE}
-            style={styles.map}
-            initialRegion={{
-              latitude: LATITUDE,
-              longitude: LONGITUDE,
-              latitudeDelta: LATITUDE_DELTA,
-              longitudeDelta: LONGITUDE_DELTA,
-            }}
-          />
+   <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        customMapStyle
+      >
+        <Marker
+          draggable
+          coordinate={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+          }}
+          onDrag={(e) => alert(JSON.stringify.apply(e.nativeEvent.coordinate))}
+          title={"Tb Gate"}
+          description={"NHF, Tb Gate"}
+        />
+      </MapView>
     
     </View>
   );
