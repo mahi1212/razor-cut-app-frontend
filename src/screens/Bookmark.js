cc53b12f45b33b2f136fe2d7eaf601d404fbe62f
mahi1212 <mahi121.mr@gmail.com>
2022-11-24 22:44:43 +0600

    bookmark removing properly

--- src/screens/Bookmark.js
+++ src/screens/Bookmark.js
@@ -1,9 +1,9 @@
-import { View, Pressable, FlatList, StyleSheet, Image } from 'react-native'
+import { View, Pressable, FlatList, StyleSheet, Image, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import PageHeader from '../components/Home/PageHeader/PageHeader';
 import Text from '../components/text/text';
 import { colors } from '../theme/colors';
-import { Entypo, FontAwesome, MaterialCommunityIcons, Ionicons} from '@expo/vector-icons';
+import { Entypo, FontAwesome, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
 export default function Bookmark({ route }) {
     const { cart } = route.params;
@@ -18,44 +18,56 @@ export default function Bookmark({ route }) {
                 .then(res => res.json())
                 .then(data => {
                     // setCartItems(data)
-                    // console.log(data)
                     setCartItems(prev => [...prev, data])
                 }
                 )
         })
     }, [])
-    // let removeDuplicate = [...new Set(cartItems)];
 
     return (
-        <View style={{ marginHorizontal: 5, flex: 1}}>
+        <View style={{ marginHorizontal: 5, flex: 1 }}>
             <PageHeader title="Bookmarks" />
             {   // if cartItems is not empty then render the FlatList
                 cartItems && <FlatList
                     data={cartItems}
                     keyExtractor={item => item._id}
                     renderItem={({ item }) => {
-                    const { shopImage, shopContainer, shopRating, shopInfo } = styles;
-
+                        const { shopImage, shopContainer, shopRating, shopInfo } = styles;
+                        const { _id } = item;
                         return (
                             <Pressable style={shopContainer}>
                                 <Image source={{ uri: item.image }} style={shopImage} />
                                 <View style={shopInfo}>
                                     <Text preset='title' style={{ color: colors.black }}>{item.name}</Text>
-                                    <Text preset='h2' style={{ color: colors.info, marginVertical: 10 }}><Entypo name='location-pin' color={'red'} style={{marginRight: 10}} />{item.address}</Text>
+                                    <Text preset='h2' style={{ color: colors.info, marginVertical: 10 }}><Entypo name='location-pin' color={'red'} style={{ marginRight: 10 }} />{item.address}</Text>
                                     <View style={shopRating}>
                                         <FontAwesome name="star" size={14} color={'green'} />
-                                        <Text preset='h2' style={{ color: colors.darkOrange, marginHorizontal: 5 }}>{item.rating.reduce((a, b)=> a+b)/ item.rating.length}</Text>
+                                        <Text preset='h2' style={{ color: colors.darkOrange, marginHorizontal: 5 }}>{item.rating.reduce((a, b) => a + b) / item.rating.length}</Text>
                                     </View>
                                 </View>
                                 {/* delete button */}
-                                <Pressable onPress={()=> {
-                                    console.log("pressed removed")
-                                } }
-                                style={{backgroundColor: 'red', padding: 10, marginRight: 10, borderRadius: 10}}>
+                                <Pressable onPress={() => {
+                                    console.log(cartItems, 'removed')
+                                    Alert.alert(
+                                        "Remove shop",
+                                        "Are you sure to remove?",
+                                        [
+                                            {
+                                                text: "Cancel",
+                                                onPress: () => console.log("Cancel Pressed"),
+                                                style: "cancel"
+                                            },
+                                            {
+                                                text: "OK", onPress: () => {
+                                                    setCartItems((items) => items.filter(item => item._id !== _id));
+                                                }
+                                            }
+                                        ]
+                                    );
+                                }}
+                                    style={{ backgroundColor: 'red', padding: 10, marginRight: 10, borderRadius: 10 }}>
                                     <Ionicons name="trash-outline" size={24} color="white" />
                                 </Pressable>
-                                
-                                
                             </Pressable>
                         )
                     }
@@ -73,7 +85,7 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 5,
         borderBottomLeftRadius: 5,
         marginRight: 10,
-        
+
     },
     shopInfo: {
         flex: 1,
@@ -98,5 +110,5 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#EEEEEE',
     }
-    
+
 })
