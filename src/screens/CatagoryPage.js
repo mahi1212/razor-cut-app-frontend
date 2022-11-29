d0a94b8b900a4b808551417c5fee1684784cd0d8
mahi1212 <mahi121.mr@gmail.com>
2022-11-30 01:16:54 +0600

    catagoryPage with searching applied

--- src/screens/CatagoryPage.js
+++ src/screens/CatagoryPage.js
@@ -12,41 +12,39 @@ export default function CatagoryPage({ route }) {
     const [searchResults, setSearchResults] = React.useState([])
     const [cart, setCart] = React.useState([])
     React.useEffect(() => {
-        if (searchText.length > 0) {
-            fetch(`http://192.168.0.221:5000/shops`)
-                .then(res => res.json())
-                .then(data => {
-                    setSearchResults(data)
-                }).catch(err => {
-                    console.log(err)
-                })
-        }
-    }, [searchText])
-    
+        fetch(`http://192.168.0.221:5000/shops`)
+            .then(res => res.json())
+            .then(data => {
+                setSearchResults(data)
+            }).catch(err => {
+                console.log(err)
+            })
+    }, [])
+    const searchFilterFunction = (text) => {
+        console.log(text)
+    }
     return (
         <View style={{ marginHorizontal: spacing[3] }}>
             <PageHeader title={text} />
-            <View style={{marginTop: -spacing[2]}}>
-                <Search searchText={searchText} setSearchText={setSearchText} />
+            <View style={{ marginTop: -spacing[2] }}>
+                <Search searchText={searchText} setSearchText={setSearchText} searchFilterFunction={searchFilterFunction} />
             </View>
             {
-                // shop that contains the catagory text
+                // shop that contains the catagoryName(status in database) text
                 searchResults.filter((item) => {
                     return item.status.toLowerCase().includes(text.toLowerCase())
                 }
                 ).map((item) => {
-                    return (
-                        <View style={{
-                            width: '100%',
-                            marginTop: spacing[3]
-                        }}>
-                            <SingleShop key={item.name} shop={item} cart={cart} setCart={setCart} visibleIcon={false} />
-                            {/* {item.name} */}
-                        </View>
-                    )
+                    // filter by search text
+                    if (item.name.toLowerCase().includes(searchText.toLowerCase())) {
+                        return (
+                            <SingleShop key={item.id} shop={item} cart={cart} setCart={setCart} />
+                        )
+                    }
                 })
+                // well done mahi :D
             }
-            
+
         </View>
     )
 }
