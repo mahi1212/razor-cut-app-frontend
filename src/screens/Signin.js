a73433b3b58eeaf4309a0e8c9457fdec118d42e5
mahi1212 <mahi121.mr@gmail.com>
2022-11-30 14:05:32 +0600

    auth navigation added

---
+++ src/screens/Signin.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Signin() {
+  return (
+    <View>
+      <Text>
+        Signin
+        ok
+      </Text>
+    </View>
+  )
+}
