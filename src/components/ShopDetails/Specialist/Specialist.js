99dea18e68f5ba106b0cde508bb2963dc27ee24d
mahi1212 <mahi121.mr@gmail.com>
2022-12-02 02:01:28 +0600

    added specialist

---
+++ src/components/ShopDetails/Specialist/Specialist.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Specialist() {
+  return (
+    <View>
+      <Text>Specialist</Text>
+    </View>
+  )
+}
