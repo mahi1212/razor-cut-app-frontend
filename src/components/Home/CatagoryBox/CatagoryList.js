c9290408bc65b3ee659361b67b0db2c68d5d2421
mahi1212 <mahi121.mr@gmail.com>
2022-12-04 20:44:06 +0600

    added Explore map

--- src/components/Home/CatagoryBox/CatagoryList.js
+++ src/components/Home/CatagoryBox/CatagoryList.js
@@ -42,103 +42,103 @@ export const details = [
 
 // [
 //     {
-//       "name": "Pritty Parlour",
-//       "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg",
-//       "mobile": "+8801772057255",
-//       "address": "Bondor",
-//       "street": "Jail Road, House - 32/2, Bondor, Sylhet",
-//       "latitude": 24.897,
-//       "longitude": 91.871,
-//       "website": "https://www.algostackbd.com/",
-//       "waiting": "1",
-//       "avgTime": "40",
-//       "status": "Makeup",
-//       "workingHours": ": 10:00 AM - 11:00 PM",
-//       "rating": [
-//         3,
-//         4
-//       ],
-//       "members": [
-//         {
-//           "id": "1",
-//           "name": "Nathan",
-//           "image": "https://menshaircuts.com/wp-content/uploads/2021/11/best-haircuts-for-men-short-fade-spiky-side-swept-683x1024.jpg",
-//           "position": "Sr. Barber"
-//         },
-//         {
-//           "id": "2",
-//           "name": "Sarah",
-//           "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Sarah_Bolger_%2848484781976%29_%28cropped%29.jpg/220px-Sarah_Bolger_%2848484781976%29_%28cropped%29.jpg",
-//           "position": "Makeup Artist"
-//         },
-//         {
-//           "id": "3",
-//           "name": "Saiba",
-//           "image": "https://images.mubicdn.net/images/cast_member/472121/cache-568799-1595384054/image-w856.jpg",
-//           "position": "Jr. Barber"
-//         },
-//         {
-//           "id": "4",
-//           "name": "Jenny",
-//           "image": "https://www.sei.org/wp-content/uploads/2019/09/jenny-han-e1568180419685.jpg",
-//           "position": "Hair Stylist"
-//         },
-//         {
-//           "id": "5",
-//           "name": "Mark",
-//           "image": "https://i.mydramalist.com/QL1o6_5c.jpg",
-//           "position": "Sr. Barber"
-//         }
-//       ],
-//       "about": "This is about us",
-//       "package": [
-//         {
-//           "name": "Wedding Package",
-//           "price": "5000",
-//           "time": "2",
-//           "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
-//         },
-//         {
-//           "name": "Bridal Makeup",
-//           "price": "7500",
-//           "time": "3",
-//           "image": "https://i.ibb.co/L8LCJ2Q/delfina-pan-w-Jo-B8-D3hnzc-unsplash.jpg"
-//         },
-//         {
-//           "name": "Tourist Choice",
-//           "price": "2500",
-//           "time": "1",
-//           "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
-//         }
-//       ],
-//       "gellary": [
-//         {
-//           "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
-//         },
-//         {
-//           "image": "https://i.ibb.co/L8LCJ2Q/delfina-pan-w-Jo-B8-D3hnzc-unsplash.jpg"
-//         },
-//         {
-//           "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
-//         },
-//         {
-//           "image": "https://i.ibb.co/L8LCJ2Q/delfina-pan-w-Jo-B8-D3hnzc-unsplash.jpg"
-//         }
-//       ],
-//       "review": [
-//         {
-//           "name": "Nathan",
-//           "image": "https://menshaircuts.com/wp-content/uploads/2021/11/best-haircuts-for-men-short-fade-spiky-side-swept-683x1024.jpg",
-//           "description": "Not bad at all, I like it"
-//         },
-//         {
-//           "name": "Test",
-//           "image": "https://images.mubicdn.net/images/cast_member/472121/cache-568799-1595384054/image-w856.jpg",
-//           "description": "Service is good"
-//         }
-//       ]
+//         "name": "Pritty Parlour",
+//         "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg",
+//         "mobile": "+8801772057255",
+//         "address": "Bondor",
+//         "street": "Jail Road, House - 32/2, Bondor, Sylhet",
+//         "latitude": 24.897,
+//         "longitude": 91.871,
+//         "website": "https://www.algostackbd.com/",
+//         "waiting": "1",
+//         "avgTime": "40",
+//         "status": "Makeup",
+//         "workingHours": ": 10:00 AM - 11:00 PM",
+//         "rating": [
+//             3,
+//             4
+//         ],
+//         "members": [
+//             {
+//                 "id": "1",
+//                 "name": "Nathan",
+//                 "image": "https://menshaircuts.com/wp-content/uploads/2021/11/best-haircuts-for-men-short-fade-spiky-side-swept-683x1024.jpg",
+//                 "position": "Sr. Barber"
+//             },
+//             {
+//                 "id": "2",
+//                 "name": "Sarah",
+//                 "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Sarah_Bolger_%2848484781976%29_%28cropped%29.jpg/220px-Sarah_Bolger_%2848484781976%29_%28cropped%29.jpg",
+//                 "position": "Makeup Artist"
+//             },
+//             {
+//                 "id": "3",
+//                 "name": "Saiba",
+//                 "image": "https://images.mubicdn.net/images/cast_member/472121/cache-568799-1595384054/image-w856.jpg",
+//                 "position": "Jr. Barber"
+//             },
+//             {
+//                 "id": "4",
+//                 "name": "Jenny",
+//                 "image": "https://www.sei.org/wp-content/uploads/2019/09/jenny-han-e1568180419685.jpg",
+//                 "position": "Hair Stylist"
+//             },
+//             {
+//                 "id": "5",
+//                 "name": "Mark",
+//                 "image": "https://i.mydramalist.com/QL1o6_5c.jpg",
+//                 "position": "Sr. Barber"
+//             }
+//         ],
+//         "about": "This is about us",
+//         "package": [
+//             {
+//                 "name": "Wedding Package",
+//                 "price": "5000",
+//                 "time": "2",
+//                 "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
+//             },
+//             {
+//                 "name": "Bridal Makeup",
+//                 "price": "7500",
+//                 "time": "3",
+//                 "image": "https://i.ibb.co/L8LCJ2Q/delfina-pan-w-Jo-B8-D3hnzc-unsplash.jpg"
+//             },
+//             {
+//                 "name": "Tourist Choice",
+//                 "price": "2500",
+//                 "time": "1",
+//                 "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
+//             }
+//         ],
+//         "gellary": [
+//             {
+//                 "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
+//             },
+//             {
+//                 "image": "https://i.ibb.co/L8LCJ2Q/delfina-pan-w-Jo-B8-D3hnzc-unsplash.jpg"
+//             },
+//             {
+//                 "image": "https://i.ibb.co/9Z7gp0G/engin-akyurt-g-m8-EDc4-X6-Q-unsplash.jpg"
+//             },
+//             {
+//                 "image": "https://i.ibb.co/L8LCJ2Q/delfina-pan-w-Jo-B8-D3hnzc-unsplash.jpg"
+//             }
+//         ],
+//         "review": [
+//             {
+//                 "name": "Nathan",
+//                 "image": "https://menshaircuts.com/wp-content/uploads/2021/11/best-haircuts-for-men-short-fade-spiky-side-swept-683x1024.jpg",
+//                 "description": "Not bad at all, I like it"
+//             },
+//             {
+//                 "name": "Test",
+//                 "image": "https://images.mubicdn.net/images/cast_member/472121/cache-568799-1595384054/image-w856.jpg",
+//                 "description": "Service is good"
+//             }
+//         ]
 //     }
-//   ]
+// ]
 // all data old
 // [
 //     {
--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,10 +1,13 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Linking, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { spacing } from '../theme/spacing'
 import { colors } from '../theme/colors'
 import MapView, { Marker, } from 'react-native-maps'
 import * as Location from 'expo-location';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
+import { SearchBar } from 'react-native-screens'
+import Search from '../components/Home/Search/Search'
 
 export default function Explore() {
   const [region, setRegion] = React.useState({
@@ -29,25 +32,101 @@ export default function Explore() {
       setRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        // it difines how much you can zoom map view
+        // it difines how much you can zoom in map view
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0442,
       })
     })();
   }, []);
 
+  const [shops, setShops] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [searchText, setSearchText] = React.useState('')
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    // if (text) {
+    //   // Inserted text is not blank
+    //   // Filter the masterDataSource
+    //   // Update FilteredDataSource
+    //   const newData = data.filter(function (item) {
+    //     const itemData = item.title
+    //       ? item.title.toUpperCase()
+    //       : ''.toUpperCase();
+    //     const textData = text.toUpperCase();
+    //     return itemData.indexOf(textData) > -1;
+    //   });
+    //   console.log(newData)
+    //   // setFilteredDataSource(newData);
+    //   setSearchText(text);
+    // }
+  }
+  const getShops = () => {
+    setIsLoading(true)
+    fetch(`http://192.168.0.121:5000/shops`)
+      .then(res => res.json())
+      .then(data => {
+        setShops(data)
+      }).catch(err => {
+        console.log(err)
+      }).finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    getShops();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, marginHorizontal: spacing[1] }}>
-      <MapView
-        style={{ flex: 1 }}
-        region={region}
-      >
-        <Marker
-          coordinate={region}
-          title={"name"}
-          description={"address"}
-        />
-      </MapView>
+      {/* select shop address in map and return to this */}
+      {
+        <MapView
+          style={{ flex: 1 }}
+          region={region}
+          showsUserLocation={true}
+          showsCompass={true}
+          showsScale={true}
+        >
+          {shops && shops.map((item, index) => (
+            <Marker
+              key={index}
+              // image={{uri: item.image}}
+              image={require('../../assets/images/shop-marker.png')}
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+              title={item.name}
+              description={item.address}
+            // 
+            // onPress={() => {
+            //   Linking.openURL(`https://www.google.com/maps/search/?api=1&query=${item.latitude},${item.longitude}`)
+            // }}
+
+            />
+          ))}
+          <Marker
+            image={require('../../assets/images/user-marker.png')}
+            coordinate={{
+              latitude: region.latitude,
+              longitude: region.longitude,
+            }}
+
+            title="You are here"
+          />
+          {/* show direaction to selected place */}
+          {/* <MapViewDirections
+    origin={region}
+    destination={selectedShop} //here will be selected place by user
+    apikey={GOOGLE_MAPS_APIKEY} //we have no api key so it will not work
+    strokeWidth={3}
+    strokeColor="hotpink"
+  /> */}
+
+        </MapView>
+      }
+
     </SafeAreaView>
   )
 }
